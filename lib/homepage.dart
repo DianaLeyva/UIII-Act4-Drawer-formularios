fb51662cba88a58b5540cf2ccaba0a4bf6cde234
a.22308051281071 <a.22308051281071@cbtis128.edu.mx>
2025-05-29 15:52:58 +0000

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.2,
+        title: Text("Formulario Tablas"),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 117, 22, 15),
+        actions: <Widget>[
+          InkWell(
+            onTap: null,
+            child: IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ), onPressed: () {  },
+            ),
+          ),
+          InkWell(
+            onTap: null,
+            child: IconButton(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ), onPressed: () {  },
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: const Color.fromARGB(255, 117, 22, 15)),
+              accountName: Text("Diana Leyva 1071"), 
+              accountEmail: Text("diana@gmail.com"),
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  child: Text(
+                    "AM",
+                    style: TextStyle(
+                        color: Colors.pink,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  backgroundColor: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
